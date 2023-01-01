b3b4958287338b067912610a5c442c2f1d0127c3
Corrine Tan <26373533+CorrineTan@users.noreply.github.com>
2023-01-02 10:45:18 +1100

    Update leetcode239.java

---
+++ Stack+Queue/leetcode239.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        // boundary: return empty list[]
+        if(nums.length == 0 || k == 0) {
+            return new int[0];
+        }
+
+        Deque<Integer> deque = new LinkedList<>();
+        int[] res = new int[nums.length - k + 1];
+
+        for (int i = 0; i < k; i++) {
+            while(!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+        }
+        res[0] = deque.peekFirst();
+
+        for (int i = k; i < nums.length; i++) {
+            if(deque.peekFirst() == nums[i - k]) {
+                deque.removeFirst();
+            }
+            while(!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+            res[i - k + 1] = deque.peekFirst();
+        }
+        return res;
+    }
+}
